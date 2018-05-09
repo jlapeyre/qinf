9906c11c46a95d5e70c5e87305fc44723794044f
John Lapeyre <lapeyre.math122a@gmail.com>
2018-05-10 00:41:00 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # Maxima's search path. It would be a good idea to
 # store them in a subdirectory
 
-FILES=' qinf.mac qinf.lisp qinf_utils.mac log2.mac  mmacompat.mac mmacompat_lisp.lisp '
+FILES=' qinf.mac qinf.lisp qinf_utils.mac log2.mac  mmacompat.mac '
 
 mkdir ${HOME}/.maxima
 echo "cd src && cp -a ${FILES} ${HOME}/.maxima"
